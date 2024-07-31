5719b4eca4356c73c552df917ae0f014f804e9b5
Drew <54420835+Fastening@users.noreply.github.com>
2024-07-31 14:16:03 -0500

    Update valo-riot-taskkill.bat
    MODIFIED code. Added many commented out lines including comments, timeout, and pause.

--- windows/valo-riot-taskkill.bat
+++ windows/valo-riot-taskkill.bat
@@ -1,22 +1,30 @@
 @echo off
 echo Closing Valorant and associated bootstrap executables...
 
+:: Do you need to run this script as an admin?
+
 :: Close Valorant main game executable
 taskkill /F /IM VALORANT-Win64-Shipping.exe
 timeout /t 3 /nobreak
 taskkill /F /IM VALORANT.exe
 
 :: REMOVE THE DOTS TO ENABLE THE COMMANDS
+:: You may or may not need the timeout command to add a delay in tasks.
 
 :: Close Riot Client services
 :: taskkill /F /IM RiotClientServices.exe
+:: timeout /t 3 /nobreak
 
 :: Close the bootstrapper
 :: taskkill /F /IM RiotClientUx.exe
+:: timeout /t 3 /nobreak
 :: taskkill /F /IM RiotClientUxRender.exe
+:: timeout /t 3 /nobreak
 
 :: Close Vanguard anti-cheat (you might have to include this depending on how Valorant launches Vanguard)
 :: taskkill /F /IM vgtray.exe
+:: timeout /t 3 /nobreak
 :: taskkill /F /IM vgc.exe
 
 echo Done.
+:: pause
